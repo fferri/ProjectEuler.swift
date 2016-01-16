61c3173e0eb7d80ce8013f2c2934843fd30e7e53
Federico Ferri <federico.ferri.it@gmail.com>
2016-01-16 14:33:43 +0100

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Federico Ferri on 16/01/2016.
+//  Copyright © 2016 Federico Ferri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
